dccad0feb7ba01c690af076b655dfc656fe3b802
123here123 <dnzyusufcan@gmail.com>
2026-04-21 17:58:44 +0300

    Update FastUse.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/FastUse.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/FastUse.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
- * Copyright (c) Meteor Development.
+ * Copyright (c) Shadow/Meteor Development.
  */
 
 package meteordevelopment.meteorclient.systems.modules.player;
